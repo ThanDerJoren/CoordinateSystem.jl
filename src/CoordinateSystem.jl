2182c96ec72d6be09ef8a78be8c01213c42cbf0e
ThanDerJoren <ThanDerJoren@users.noreply.github.com>
2022-10-05 13:35:06 +0200

    Files generated by PkgTemplates

---
+++ src/CoordinateSystem.jl
@@ -0,0 +1,5 @@
+module CoordinateSystem
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using CoordinateSystem
+using Test
+
+@testset "CoordinateSystem.jl" begin
+    # Write your tests here.
+end
